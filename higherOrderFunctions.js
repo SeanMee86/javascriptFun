064ca5ee5161e1ea3a0e0b4c3af7ee327c9966df
hss-smee <sean@healthcaresuccess.com>
2019-09-16 16:45:50 -0700

    finished high order functions

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -12,28 +12,83 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-//forEach
+// for(let i = 0; i < companies.length; i++) {
+//     console.log(companies[i]);
+// }
 
+// ****** ForEach ******
 
+// companies.forEach((company, index, companies) => console.log(company.name));
 
-//filter
+// ****** Filter ******
 
+// const canDrink = ages.filter(age => age >= 21);
+// console.log(canDrink);
 
+// Filter Retail Companies
 
-//map
+// const retailCompanies = companies.filter(company => company.category === 'Retail');
+// console.log(retailCompanies);
 
+// Get 80's Companies
 
+// const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
+// eightiesCompanies.forEach(company => console.log(company.name));
 
-//sort
+// Get companies that lasted 10 years or more
 
+// const tenYearCompanies = companies.filter(company => (company.end - company.start) >= 10);
+// console.log(tenYearCompanies);
 
+// ****** Map ******
 
-//reduce
+// Create array of company names
 
+// const companyInfo = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+// console.log(companyInfo);
 
+// const agesSquare = ages
+//     .map(age => Math.pow(age,2))
+//     .map(age => age / 2);
+// console.log(agesSquare);
 
+// ****** Sort ******
 
+// const sortedCompanies = companies.sort(function(c1,c2){
+//     if(c1.start > c2.start){
+//         return 1;
+//     }else {
+//         return -1;
+//     }
+// });
 
-//Using reduce to get the average of an array of numbers
-let agesAverage = ages.reduce((a,v,i) => i !== ages.length-1 ? a+v : (a+v)/ages.length-1);
-console.log(agesAverage);
+// const sortedCompanies = companies.sort((a,b) => a.start > b.start ? 1 : -1);
+// console.log(sortedCompanies);
+//
+// const sortedAges = ages.sort((a,b) => a - b);
+// console.log(sortedAges);
+
+// ****** Reduce ******
+
+// const ageSum = ages.reduce((acc,val) => {
+//     console.log(`Accumulator: ${acc}, Value: ${val}`);
+//     return val !== 16 ? acc + val : acc;
+// });
+// console.log(ageSum);
+
+// const totalYears = companies.reduce((acc, val) => acc + (val.end - val.start), 0);
+// console.log(totalYears);
+
+// Using reduce to get the average of an array of numbers
+// let agesAverage = ages.reduce((a,v,i) => i !== ages.length-1 ? a+v : (a+v)/ages.length-1);
+// console.log(agesAverage);
+
+// Combining Methods
+
+// const combined = ages
+//     .map(age => age *2)
+//     .filter(age => age >= 40)
+//     .sort((a, b) => a-b)
+//     .reduce((acc, val) => acc + val);
+//
+// console.log(combined);
---
+++ till.js
@@ -0,0 +1,26 @@
+function tickets(peopleInLine){
+    console.log(peopleInLine);
+    var till = [];
+    for(var i = 0; i < peopleInLine.length; i++){
+        var person = peopleInLine[i];
+        if(person >= 25){
+            till.push(person);
+            if(person > 25) {
+                for(var j = 0; j < till.length; j++){
+                    var bill = till[j];
+                    if((person - bill) > 0){
+                        person -= bill;
+                        till.splice(j, 1);
+                        j--
+                    }
+                }
+            }
+            if(person !== 25){
+                return 'NO';
+            }
+        }
+    };
+    return 'YES';
+}
+
+console.log(tickets([[ 25, 25, 25, 25, 50, 100, 50 ]]));

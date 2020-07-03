0982b94f343902e97ac670c6806d610ef19090f8
hss-smee <seanmee8186@gmail.com>
2020-07-02 18:42:48 -0700

    updating git

--- getSite.js
+++ getSite.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-   console.log($.get('https://starvalleyhealth.org/providers/'));
+   // console.log($.get('https://starvalleyhealth.org/providers/'));
+   function callAjax(url){
+      $.ajax({
+         url,
+         method: 'GET',
+         success: function (res){
+            return res;
+         }
+      })
+   }
+
+   async function asyncFunction(){
+      return await callAjax('https://jsonplaceholder.typicode.com/posts');
+   }
+
+   console.log(asyncFunction().then(res => console.log(res)))
 });
